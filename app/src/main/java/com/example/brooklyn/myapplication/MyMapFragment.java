fef639872649a12d60d1e530c489f02548089611
Gleb Morozov <justyfai@gmail.com>
2017-03-06 12:41:22 +0700

    Created custom map fragment

---
+++ app/src/main/java/com/example/brooklyn/myapplication/MyMapFragment.java
@@ -0,0 +1,42 @@
+package com.example.brooklyn.myapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.google.android.gms.maps.MapView;
+
+public class MyMapFragment extends Fragment {
+    MapView mapView;
+    public MyMapFragment() {
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_my_map, container, false);
+        mapView = (MapView) view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        return view;
+    }
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+}
